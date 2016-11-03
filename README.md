278fb3ccc248e557cf74f8907e4002587f48625d
Andrew Beers <andybeers@gmail.com>
2016-11-03 14:19:36 -0700

    Added test files. Linked second varietal resource to DB

---
+++ README.md
@@ -0,0 +1,10 @@
+Express-Mongoose-Mongo API
+
+/roasters is the endpoint for Portland coffee roasters.
+/varietals is the endpoint for coffee varietals.
+
+GET /
+GET /:id
+POST /
+PUT /:id
+DELETE /:id
---
+++ test/api-e2e-test.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server.js');
+const assert = chai.assert;
+chai.use(chaiHttp);
+
+describe('RESTful API for two resources', () => {
+
+  before(done => {
+    
+  });
+
+  it('Does something', () => {
+
+  });
+
+  it('Does something', () => {
+
+  });
+
+  it('Does something', () => {
+
+  });
+  
+});
